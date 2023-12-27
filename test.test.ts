d6de3236bd1a2aadda1988caa0794a528c4dd61d
Inácio Mattos <inaciotm@hotmail.com>
2023-12-27 14:41:38 -0300

    fix ts!

--- test.test.ts
+++ test.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from 'bun:test'
-import StreamDecoder from '.'
+import StreamDecoder from './index'
 
 test('decoding array', () => {
   const decoder = new StreamDecoder()
@@ -23,8 +23,6 @@ test('decoding array', () => {
   const encoded = '!@#$12ka@a3sll_START[{"path":"points_of_interest.[0].category.[0]","type":"string","delta":""}]!@#$12ka@a3sll_END'
 
   const decoded = decoder.decode(encoded)
-  console.log(decoded);
-  
-  expect(typeof decoded.points_of_interest[0]).toBe('object')
+  expect(typeof decoded?.points_of_interest[0]).toBe('object')
   expect(decoded).toEqual(expected)
 })
